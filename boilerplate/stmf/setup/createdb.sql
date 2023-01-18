147b0311fc7aa44cf288fb881f240e20d620488e
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-01-18 13:12:20 -0600

    fixup sql script with semicolons

--- boilerplate/stmf/setup/createdb.sql
+++ boilerplate/stmf/setup/createdb.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE Books(bookId INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY(START WITH 1, INCREMENT BY 1),
                        title VARCHAR(30) NOT NULL,
                        isbn VARCHAR(20) NOT NULL,
-                       amount DECIMAL(10 , 2) NOT NULL, PRIMARY KEY (bookId))
+                       amount DECIMAL(10 , 2) NOT NULL, PRIMARY KEY (bookId));
 
-INSERT INTO BOOKS(title, isbn, amount) VALUES ('Learn Quantum Computing with Python', 9781838981006, 41.99)
+INSERT INTO BOOKS(title, isbn, amount) VALUES ('Learn Quantum Computing with Python', 9781838981006, 41.99);
