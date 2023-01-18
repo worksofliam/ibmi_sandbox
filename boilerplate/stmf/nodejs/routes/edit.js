928d0b3c0994e1e3dd0b0b1d4a58f813e531f964
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2023-01-17 20:27:50 -0600

    More cleanup

--- boilerplate/stmf/nodejs/routes/edit.js
+++ boilerplate/stmf/nodejs/routes/edit.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const ibmi = require('../ibmi_utils.js');
 
 router.get('/', async function(req, res) {
-    ibmi.runSql('SELECT * FROM BOOKSTORE.BOOKS', function(err, result) {
+    ibmi.runSql('SELECT * FROM BOOKS', function(err, result) {
         if (err) {
             err = 'failed to load books data. Error: ' + err;
             result = [];
--- boilerplate/stmf/nodejs/routes/index.js
+++ boilerplate/stmf/nodejs/routes/index.js
@@ -3,8 +3,7 @@ const ibmi = require('../ibmi_utils.js');
 
 //Respond with Static Book Table
 router.get('/', async function(req, res) {
-    console.log("hhhehehe"); 
-    ibmi.runSql('SELECT * FROM BOOKSTORE.BOOKS', function(err, result) {
+    ibmi.runSql('SELECT * FROM BOOKS', function(err, result) {
         res.render('staticTable.ejs', {title: 'View Books', data: result, error: null});
     })
 });
